d0103a8bc8327567ffdb590f280891b2c447b074
BrianMad7 <brianmadapathage@gmail.com>
2026-03-05 15:23:36 +0100

    Create function to get highest cards

--- src/poker.ts
+++ src/poker.ts
@@ -1,5 +1,12 @@
 import { Card, HandResult } from './types';
 
-export function evaluateHand() {
-    
+export function evaluateHand(hand: Card[], board: Card[]): HandResult {
+    const cards = [...hand, ...board];
+
+    const cardSorted = cards.sort((a, b) => b.rank - a.rank)
+
+    return {
+        category: 'High card',
+        chosen5: cardSorted.slice(0, 5)
+    }
 }
