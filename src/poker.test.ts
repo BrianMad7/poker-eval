abe577c0f671820ba32a17c2e8d3ee6382a68650
BrianMad7 <brianmadapathage@gmail.com>
2026-03-05 15:30:56 +0100

    Create test to get one pair condition

--- src/poker.test.ts
+++ src/poker.test.ts
@@ -24,3 +24,28 @@ describe('Evaluate that only high card are selected', () => {
     expect(result.chosen5[4].rank).toBe(4);
   });
 });
+
+it('should detect a Pair and include the 3 best kickers', () => {
+  const holeCards: Card[] = [
+    { rank: 10, suit: 'P' },
+    { rank: 10, suit: 'C' }
+  ];
+  const board: Card[] = [
+    { rank: 14, suit: 'K' },
+    { rank: 8, suit: 'T' },
+    { rank: 6, suit: 'P' },
+    { rank: 4, suit: 'C' },
+    { rank: 2, suit: 'K' }
+  ];
+
+  const result = evaluateHand(holeCards, board);
+
+  expect(result.category).toBe('One pair');
+
+  expect(result.chosen5[0].rank).toBe(10);
+  expect(result.chosen5[1].rank).toBe(10);
+
+  expect(result.chosen5[2].rank).toBe(14);
+  expect(result.chosen5[3].rank).toBe(8);
+  expect(result.chosen5[4].rank).toBe(6);
+});
